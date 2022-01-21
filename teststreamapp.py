6b5764d4d6973041bc70fe7b4b0c4713fe2a8e30
mahout83 <nareshpolisetti@gmail.com>
2022-01-21 13:06:23 -0600

    Update teststreamapp.py

--- teststreamapp.py
+++ teststreamapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 #import matplotlib.pyplot as plt
-print("What is Going on")
+#print("What is Going on")
 a = [23,44,55,66,89,-2,-22,-55]
-q = plt.show(a)
-st.write(q)
+#q = plt.show(a)
+st.write(a)
