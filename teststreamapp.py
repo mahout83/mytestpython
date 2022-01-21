29eb1016feca43aa99982a3b1f8fd4a584f09d16
mahout83 <nareshpolisetti@gmail.com>
2022-01-21 11:59:33 -0600

    Update teststreamapp.py

--- teststreamapp.py
+++ teststreamapp.py
@@ -1,7 +1,7 @@
-#import streamlit as st
-#st.write("This is so much fun")
+import streamlit as st
 import pandas as pd
 #import matplotlib.pyplot as plt
 print("What is Going on")
-#a = [23,44,55,66,89,-2,-22,-55]
-#plt.show(a)
+a = [23,44,55,66,89,-2,-22,-55]
+q = plt.show(a)
+st.write("This is so much fun")
