7dcdc4066773c70a7e69952eb68f7e5f769a02fb
mahout83 <nareshpolisetti@gmail.com>
2022-01-21 11:37:44 -0600

    Update teststreamapp.py

--- teststreamapp.py
+++ teststreamapp.py
@@ -1,7 +1,7 @@
 #import streamlit as st
 #st.write("This is so much fun")
 import pandas as pd
-import matplotlib.pyplot as plt
-print(dir(plt))
+#import matplotlib.pyplot as plt
+print(dir(pd))
 #a = [23,44,55,66,89,-2,-22,-55]
 #plt.show(a)
